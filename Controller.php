b0676d1c2d1d3f8b63e2fc0ab69f29f5d95d6718
erleibiazzio <erleibiazzio@gmail.com>
2025-01-13 23:54:22 -0300

    Implementa endpoint para envio de e-mail de teste

--- Controller.php
+++ Controller.php
@@ -2,9 +2,15 @@
 
 namespace OneClick;
 
+use InvalidArgumentException;
+use Exception;
 use \MapasCulturais\App;
-
+use MapasCulturais\Exceptions\MailTemplateNotFound;
+use MapasCulturais\Exceptions\NotFound;
 use MapasCulturais\Traits\ControllerAPI;
+use RuntimeException;
+use TypeError;
+use Throwable;
 
 class Controller  extends \MapasCulturais\Controllers\EntityController
 {
@@ -16,7 +22,14 @@ class Controller  extends \MapasCulturais\Controllers\EntityController
         $this->entityClassName = '\OneClick\Settings';
     }
 
-    public function GET_steps()
+    /**
+     * @return void 
+     * @throws RuntimeException 
+     * @throws InvalidArgumentException 
+     * @throws NotFound 
+     * @throws Exception 
+     */
+    public function GET_steps(): void
     {
         $app = App::i();
         
@@ -28,5 +41,44 @@ class Controller  extends \MapasCulturais\Controllers\EntityController
 
         $this->render('settings', []);
     }
+
+
+    /**
+     * @return void 
+     * @throws RuntimeException 
+     * @throws InvalidArgumentException 
+     * @throws NotFound 
+     * @throws Exception 
+     * @throws MailTemplateNotFound 
+     * @throws TypeError 
+     * @throws Throwable 
+     */
+    public function POST_sendMailTest(): void
+    {
+        $app = App::i();
+
+        $this->requireAuthentication();
+
+        if(!$app->user->is('admin')) {
+            $app->pass();
+        }
+
+        $email = $this->data['email'];
+        $params = [
+            'siteName' => $app->siteName
+        ];
+
+        $message = $app->renderMailerTemplate('email_teste_settings', $params);
+        $email_params = [
+            'from' => $app->config['mailer.from'],
+            'to' =>$email,
+            'subject' => $message['title'],
+            'body' => $message['body'],
+        ];
+
+        $send = $app->createAndSendMailMessage($email_params);
+        $this->json($send);
+    }
+    
 
 }
