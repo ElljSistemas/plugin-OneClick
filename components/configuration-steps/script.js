aa337b2cb3527bc8bb0eb549eb71c5481a4a0711
erleibiazzio <erleibiazzio@gmail.com>
2025-02-10 07:58:45 -0300

    Defne a referência no menu entidades

--- components/configuration-steps/script.js
+++ components/configuration-steps/script.js
@@ -22,7 +22,7 @@ app.component('configuration-steps', {
             ],
             'text-image': [
                 { label: 'Banner', isActive: true, submenu: [], ref: "banner", useActions: true },
-                { label: 'Entidades', isActive: false, submenu: [], useActions: true },
+                { label: 'Entidades', isActive: false, submenu: [], ref: "entities", useActions: true },
                 { label: 'Em destaque', isActive: false, submenu: [], ref: "emdestaque", useActions: true },
                 { label: 'Cadastre-se', isActive: false, submenu: [], ref: "cadastrese", useActions: true },
                 { label: 'Mapa', isActive: false, submenu: [], ref: "mapa", useActions: true },
